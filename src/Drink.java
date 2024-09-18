d73a74e4bf05f476e6f068589ebc14ebbe09a889
liv.sundman <liv.sundman@ga.ntig.se>
2024-09-18 13:14:02 +0200

    forst

--- src/Drink.java
+++ src/Drink.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Drink {
+public class Drink {
+    // Attributes
+    String name;
+    String flavour;
+    float volume;
+
+    public void takeADrink() {
+        volume = volume - 30;
+        System.out.println("There is now " + volume + " ml left of the " + name);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Drink drink1 = new Drink();
+
+        drink1.name = "coca cola";
+        drink1.flavour = "cola";
+        drink1.volume = 330;
+
+        System.out.println(drink1.name + " tastes like " + drink1.flavour + " and is " + drink1.volume + " ml");
+
+
+        Drink drink2 = new Drink();
+        drink2.name = "Fanta";
+        drink2.flavour = "oranges";
+        drink2.volume = 1500;
+
+        System.out.println(drink2.name + " tastes like " + drink2.flavour + " and is " + drink2.volume + " ml");
+
+        drink1.volume = drink1.volume - 30;
+        System.out.println("You drink 30 ml from " + drink1.name + " there is " + drink1.volume + " left");
+
+        drink2.takeADrink();
+
+
     }
 }
